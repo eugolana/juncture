e981595678fb5ba7981a149798173825cdea982e
Sam Sharp <eugolana2@gmail.com>
2017-11-15 15:48:49 +0000

    tidied app.js, changing vars to lets and consts

--- page/scripts/app.js
+++ page/scripts/app.js
@@ -1,23 +1,24 @@
-var ipfsURL = window.location.protocol + '//' + window.location.host + '/ipfs/';
+const ipfsURL = window.location.protocol + '//' + window.location.host + '/ipfs/';
 
 
-var emptyDir = "QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn";
-var	editableElements = ['content','title', 'childA', 'childB'];
+const emptyDir = "QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn";
+const	editableElements = ['content','title', 'childA', 'childB'];
 
-// get this page hash from url
-var sel = location.pathname.split('ipfs/')[1] || 'offchain'
-sel = sel.split('/')[0]
+// get this page hash from url.
+// ( assumes lcoation.pathname is in form /ipfs/<ipfs hash>)/
+const sel = location.pathname.split('/')[2] || 'offchain'
+
 // sel (this pages hash) and j (th contract instance) are in global scope
 
-var j;
+let j;
 
-var editMode = true;
-var cssChanged = false;
-var newCSSName;
-var newCSSText;
-var oldCSSName;
+let editMode = true;
+let cssChanged = false;
+let newCSSName;
+let newCSSText;
+let oldCSSName;
 
-var qs = parseQuery(window.location.search)
+const qs = parseQuery(window.location.search)
 
 window.onload = function() {
 	// check if we are in edit mode.
@@ -28,10 +29,8 @@ window.onload = function() {
 	}
 
 	// init web3
-    var Juncture = web3.eth.contract(abi);
+    let Juncture = web3.eth.contract(abi);
 	j = Juncture.at(contractAddress);
-	console.log("sucessfully made contract instance");
-	console.log(j);
 
 	// Load child addresses if they exist
 	// otherwise will make edit page link
@@ -69,7 +68,7 @@ function removeEditElements(_editableElements){
 
 function makeFieldsEditable(_editableElements){
 	// enters edit mode (doesn't make individual elements 'contentEditable')
-	for (var i = 0; i < _editableElements.length; i++) {
+	for (let i = 0; i < _editableElements.length; i++) {
 		let el = document.getElementById(_editableElements[i]);
 		if (!el.onchange) {
 			el.onchange = function(){}
@@ -90,7 +89,7 @@ function makeFieldsEditable(_editableElements){
 
 function makeFieldsUneditable(_editableElements){
 	// hides 'editmode' t allow pag preview on spacebar press
-	for (var i = 0; i < _editableElements.length; i++) {
+	for (let i = 0; i < _editableElements.length; i++) {
 		let el = document.getElementById(_editableElements[i]);
 		el.classList.remove('editable');
 		el.onmouseenter = function(event) {
@@ -103,7 +102,7 @@ function makeFieldsUneditable(_editableElements){
 }
 
 function turnOffAllContentEditable(_editableElements) {
-	for (var i = 0; i < _editableElements.length; i++) {
+	for (let i = 0; i < _editableElements.length; i++) {
 		document.getElementById(_editableElements[i]).contentEditable = 'false';
 		document.getElementById(_editableElements[i]).onchange();
 	}
@@ -112,13 +111,13 @@ function turnOffAllContentEditable(_editableElements) {
 function editModeKeyEvents() {
 	// enter view mode if 'space' is pressed
     document.addEventListener('keydown', (event) => {
-    	var keyName = event.key;
+    	let keyName = event.key;
     	if (keyName == ' '){
 	    	if (! editMode) {
 	    		return;
 	    	}
 	    	editMode = false;
-	    	for (var i=0; i < editableElements.length; i++) {
+	    	for (let i = 0; i < editableElements.length; i++) {
 	    		if (document.getElementById(editableElements[i]).contentEditable == 'true') {
 	    			return
 	    		}
@@ -128,13 +127,13 @@ function editModeKeyEvents() {
     	}
 	});
     document.addEventListener('keyup', (event) => {
-    	var keyName = event.key;
+    	let keyName = event.key;
   		if (keyName == ' ') {
   			if (editMode == true) {
     			return;
     		}
     		editMode = true;
-   	    	for (var i=0; i < editableElements.length; i++) {
+   	    	for (let i=0; i < editableElements.length; i++) {
 	    		if (document.getElementById(editableElements[i]).contentEditable == 'true') {
 	    			return
 	    		}
@@ -148,7 +147,7 @@ function addSaveButton() {
 	let endBits = document.getElementById('endBits');
 	let saveButton = document.createElement('div');
 	saveButton.id = "save";
-		saveButton.innerText = "Save";
+	saveButton.innerText = "Save";
 	saveButton.onclick = function(event) {
 		let file = saveSnapshot();
 		upload(file, function(hash) {
@@ -207,7 +206,7 @@ function removeEditCSS() {
 }
 
 function clearFields(_editableElements) {
-	for (var i = 0; i < _editableElements.length; i++) {
+	for (let i = 0; i < _editableElements.length; i++) {
 		document.getElementById(_editableElements[i]).innerText = "(" + _editableElements[i] + ")";
 	}
 }
@@ -229,8 +228,31 @@ function removeEditMessage() {
 // Dashboard UI stuff
 
 function initNavBar() {
+	// Check if page is on chain
+	j.pageExists(sel, function(err, res) {
+		if (err) {
+			console.log('contract not registered')
+		} else {
+			if (!res) {
+				let el = document.getElementById('nav_message');
+				el.style.display = 'block'
+				let dashboard = document.getElementById('dashboard')
+				el.innerHTML = 'Page not yet registered.. wait a while and reload :)';
+				dashboard.style['background-color'] = '#BB4444';
+				dashboard.style.height = '60px';
+				let info = document.getElementsByClassName('nav_info')
+				for(let i = 0; i < info.length; i++) {
+					info[i].style.display = 'none'
+				}
+
+			}
+
+		}
+
+	})
+
 	if (contractAddress == 'offchain') {
-		document.getElementById('nav_contractAddress').innerText = offchain;
+		document.getElementById('nav_contractAddress').innerText = 'offchain  (filler to  make it roughly the right length)';
 	} else {
 		let a = document.createElement('a');
 		a.href = "https://ropsten.etherscan.io/address/" + contractAddress;
@@ -249,19 +271,34 @@ function initNavBar() {
 		a.innerText = parent;
 		document.getElementById('nav_parentAddress').appendChild(a)
 	} else {
-		document.getElementById('nav_parentAddress').innerText = 'orphan';
+		document.getElementById('nav_parentAddress').innerText = 'orphan (with addd spacing just for testing purposes';
 	}
 	// get deposit
 	j.deposit(function(err, res) {
-		if (err) {console.log('deposit function went wrong... contract not initialised??');return}
+		if (err) {
+			console.log('deposit function went wrong... contract not initialised??');
+			return;
+		}
 		document.getElementById('nav_contractDeposit').innerText = res.toNumber();
 
+	})
+
+	// get startNode
+	j.startNode(function(err, res) {
+		if (err) {
+			console.log('deposit function went wrong... contract not initialised??');
+			return;
+		}
+		let a = document.createElement('a');
+		a.href = ipfsURL + res;
+		a.innerText = res;
+		document.getElementById('nav_startNode').appendChild(a);
 	})
 }
 
 function purgeNavBar() {
-	var navInfo = document.getElementsByClassName('nav_info');
-	for (var i = 0; i < navInfo.length; i++) {
+	let navInfo = document.getElementsByClassName('nav_info');
+	for (let i = 0; i < navInfo.length; i++) {
 		navInfo[i].getElementsByClassName('value')[0].innerHTML = '';
 	}
 }
@@ -272,36 +309,36 @@ function saveSnapshot() {
 	// This removes all traces of editor, and a few other unique elements, 
 	// from the raw html of the page, and returns the cleansed html for
 	// uploading
-	removeEditElements(editableElements)
+	removeEditElements(editableElements);
 	purgeNavBar();
 
-	var f = document.documentElement.outerHTML
+	let f = document.documentElement.outerHTML;
 	// find if we have injected scrpt
 	// (specifically, remove) metamask inpage.js or it will propagate like a cancer
-	let injectedJSStart = f.search("<script>")
+	let injectedJSStart = f.search("<script>");
 	if (injectedJSStart >= 0) {
-		injectedJSEnd = f.search("</script>")
+		injectedJSEnd = f.search("</script>");
 		f = f.slice(0,injectedJSStart) + f.slice(injectedJSEnd + 9);
 	}
 
 	// replace 'var parent'
-	let parentStart = f.search("var parent")
-	let parentEnd = parentStart + f.slice( parentStart).search(';')
-	f = f.slice(0,parentStart) + "var parent = '" + sel + "'" + f.slice(parentEnd);
+	let parentStart = f.search("const parent");
+	let parentEnd = parentStart + f.slice( parentStart).search(';');
+	f = f.slice(0,parentStart) + "const parent = '" + sel + "'" + f.slice(parentEnd);
 
 	// replace 'var child'
-	let childStart = f.search("var child")
-	let childEnd = childStart + f.slice(childStart).search(';')
-	f = f.slice(0,childStart) + "var child = " + qs['child'] + f.slice(childEnd);	
+	let childStart = f.search("const child");
+	let childEnd = childStart + f.slice(childStart).search(';');
+	f = f.slice(0,childStart) + "const child = " + qs['child'] + f.slice(childEnd);	
 
 	// replace css href=data ith style/filename.css
 	let hrefStart = f.search('href="data:');
 	if (hrefStart >= 0) {
 		let hrefEnd = f.slice(hrefStart).search(">") + hrefStart;
-		f = f.slice(0, hrefStart) + 'href="styles/' + newCSSName + '"' + f.slice(hrefEnd)
+		f = f.slice(0, hrefStart) + 'href="styles/' + newCSSName + '"' + f.slice(hrefEnd);
 	}
 	// add the edit things back
-	addEditElements(editableElements)
+	addEditElements(editableElements);
 	initNavBar();
 	return f;
 }
@@ -320,30 +357,30 @@ function upload(f, cb) {
 			method: 'PUT',
 			body: f,
 		}).then(function(response) {
-			var hash = response.headers.get('Ipfs-Hash')
+			let hash = response.headers.get('Ipfs-Hash');
 			if (cssChanged) {
 				fetch(ipfsURL + hash + "/styles/" + newCSSName, {
 				method: 'PUT',
 				body: newCSSText,
 				}).then( function(response) {
-					hash = response.headers.get('Ipfs-Hash');
+					let hash = response.headers.get('Ipfs-Hash');
 					fetch(ipfsURL + hash + "/styles/" + oldCSSName, {
 						method: 'DELETE',
 					}).then(function(response) {
 						// add new CSS to folder before uploading
 						hash = response.headers.get('Ipfs-Hash');
-						console.log("added page:" + hash)
-						cb(hash)
+						console.log("added page:" + hash);
+						cb(hash);
 					})
 				})
 			} else {
 				if (!hash) {
-					console.log('whoops, something went wrong! failed to add page')
+					console.log('whoops, something went wrong! failed to add page');
 					return
 				}
 				// no new css. Just call cb
-				console.log("added page: " + hash)
-				cb(hash)
+				console.log("added page: " + hash);
+				cb(hash);
 			} 
 		})
 	})
@@ -352,7 +389,7 @@ function upload(f, cb) {
 
 function getMyChild(j, parent, childno, id) {
   j.getchild(parent, childno, function(err, res) {
-    var el = document.getElementById(id);
+    let el = document.getElementById(id);
     if (err) {
       el.innerText = err;
     } else {
@@ -366,21 +403,25 @@ function getMyChild(j, parent, childno, id) {
 }
 
 function addPage(j, hash, parent, child) {
-	document.getElementById('editMessage').innerText = "Adding new page. address: " + hash + ". Just waiting for confirmation..."
+	let a = document.createElement('a')
+	a.href = ipfsURL + hash;
+	a.innerText = hash;
+	document.getElementById('editMessage').innerText = "Adding new page. address: " + hash + ". Just waiting for confirmation...";
+	document.getElementById('editMessage').appendChild(a);
 	j.addPage( hash, parent, child, function(err, tHash){
 		// leave these logs for now for debugging
 		if (err) {
-			console.log("addPage errored")
-			console.log(err)
+			console.log("addPage errored");
+			console.log(err);
 		} else {
 			e = j.LogNewPage();
 			e.watch(function(err, res){
 				if (res.transactionHash == tHash) {
-					let a = document.createElement('a')
-					a.href = ipfsURL + hash
-					a.innerText = hash
-					document.getElementById('editMessage').innerText = "Page successfully added!"
-					document.getElementById('editMessage').appendChild(a)
+					let a = document.createElement('a');
+					a.href = ipfsURL + hash;
+					a.innerText = hash;
+					document.getElementById('editMessage').innerText = "Page successfully added!";
+					document.getElementById('editMessage').appendChild(a);
 					e.stopWatching();
 				}
 			})
@@ -390,10 +431,10 @@ function addPage(j, hash, parent, child) {
 }
 
 function parseQuery(queryString) {
-    var query = {};
-    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (var i = 0; i < pairs.length; i++) {
-        var pair = pairs[i].split('=');
+    let query = {};
+    let pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (let i = 0; i < pairs.length; i++) {
+        let pair = pairs[i].split('=');
         query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
     }
     return query;
